e684aa4fc14ce9063a0d23f02c895ab6237a463a
auroratide <tf.auroratide@gmail.com>
2020-01-04 22:26:16 -0600

    Steady moon rotation

---
+++ Assets/Moon/Moon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moon : MonoBehaviour
+{
+    [SerializeField] private Config config;
+    public void Start()
+    {
+        StartCoroutine(SteadyRotation(config.rotationSpeed));
+    }
+
+    public IEnumerator SteadyRotation(float rotationSpeed)
+    {
+        while(true)
+        {
+            transform.Rotate(Vector3.up, rotationSpeed);
+            yield return null;
+        }
+    }
+
+    [System.Serializable] public class Config
+    {
+        public float rotationSpeed = 0.1f;
+    }
+}
