cc229ec08c24c8a663aaa9d1e36f71749b0e9f9f
Wisp <chromewisp@gmail.com>
2020-01-05 16:09:21 -0600

    Can now zap a ghost

---
+++ Assets/Zapper/Zapper.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zapper : MonoBehaviour
+{
+
+    public GameObject origin;
+
+    void Update()
+    {
+        if (IsFirstTouchDetected())
+        {
+            print("Touch Detected");
+
+            RaycastHit hit;
+            Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+
+            print(ray);
+
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                print("Found an object to hit");
+                Transform objectHit = hit.transform;
+                print(objectHit);
+
+                if (objectHit.GetComponent<Ghost>() != null)
+                {
+                    objectHit.GetComponent<Ghost>().Zap();
+                }
+            }
+        }
+    }
+
+    private bool IsFirstTouchDetected()
+    {
+        return (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            || Input.GetMouseButtonDown(0);
+    }
+}
