f6af088c54cd6ba4f37d4070ff462f8263c9eda1
Wisp <chromewisp@gmail.com>
2020-01-12 18:34:41 -0600

    Working ARCore (can see camera feed)
    https://codelabs.developers.google.com/codelabs/arcore-intro/#0
    
    Followed this "Snakes on a Plane" tutorial.

---
+++ Assets/SceneController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class SceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        QuitOnConnectionErrors();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Session.Status != SessionStatus.Tracking)
+        {
+            int lostTrackingSleepTimeout = 15;
+            Screen.sleepTimeout = lostTrackingSleepTimeout;
+            return;
+        }
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+
+    void QuitOnConnectionErrors()
+    {
+        if(Session.Status == SessionStatus.ErrorPermissionNotGranted)
+        {
+            StartCoroutine(CodelabUtils.ToastAndExit("Camera permission required to run this, y'all!", 3));
+        }
+        else if(Session.Status.IsError())
+        {
+            StartCoroutine(CodelabUtils.ToastAndExit("ARCore encountered a problem connecting. Please restart! D:", 3));
+        }
+    }
+}
