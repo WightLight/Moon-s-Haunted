1d2b76cb3c7c404122ae207d95dcad048a2ab9ca
auroratide <tf.auroratide@gmail.com>
2020-01-18 13:22:26 -0600

    A ghost can now spawn

--- Assets/Ghosts/Ghost.cs
+++ Assets/Ghosts/Ghost.cs
@@ -4,8 +4,50 @@ using UnityEngine;
 
 public class Ghost : MonoBehaviour
 {
+    private const float SPAWN_SPEED = 0.05f;
+  
+    private Vector3 originalPosition;
+
+    public void Start()
+    {
+        originalPosition = transform.localPosition;
+        Reset();
+    }
+
     public void Zap()
     {
+        Reset();
+    }
+
+    public void Spawn()
+    {
+        gameObject.SetActive(true);
+        Rise();
+    }
+
+    private void Rise()
+    {
+        StartCoroutine(RiseCoroutine());
+    }
+
+    private void Reset()
+    {
         gameObject.SetActive(false);
+        transform.localPosition = originalPosition;
+    }
+
+    private IEnumerator RiseCoroutine()
+    {
+        float t = 0;
+        Vector3 newPosition = originalPosition + transform.localScale.x * (transform.localRotation * Vector3.up);
+        print(originalPosition);
+        print(newPosition);
+
+        while(t < 1)
+        {
+            t += SPAWN_SPEED;
+            transform.localPosition = Vector3.Lerp(originalPosition, newPosition, t);
+            yield return null;
+        }
     }
 }
---
+++ Assets/Ghosts/GhostsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostsController : MonoBehaviour
+{
+    public Moon moon;
+    private List<Ghost> ghosts;
+
+    void Start()
+    {
+        ghosts = new List<Ghost>(moon.GetComponentsInChildren<Ghost>());
+    }
+
+    void Update()
+    {
+        if(NumberOfActiveGhosts() < 1) ghosts[1].Spawn();
+    }
+
+    public int NumberOfActiveGhosts()
+    {
+        return ghosts.FindAll(ghost => ghost.gameObject.activeSelf).Count;
+    }
+}
