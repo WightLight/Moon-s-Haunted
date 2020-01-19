7303d0a84a551f860e13d78319886409fb3d3cae
auroratide <tf.auroratide@gmail.com>
2020-01-19 16:19:09 -0600

    Show crosshair on start

--- Assets/Game/Game.cs
+++ Assets/Game/Game.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class Game : MonoBehaviour
 {
     public GameObject moonTemplate;
-    public Zapper zapper;
+    public GameObject zapper;
+    public GameObject crosshair;
 
     public void Begin(Pose moonPose)
     {
         Instantiate(moonTemplate, moonPose.position, moonPose.rotation);
         Debugging.Log("Object spawned");
 
-        zapper.gameObject.SetActive(true);
-        Debugging.Log("Started zapper active!");
+        zapper.SetActive(true);
+        crosshair.SetActive(true);
+        Debugging.Log("Set stuff to active!");
     }
 }
