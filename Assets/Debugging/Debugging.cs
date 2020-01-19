ea06f6ddfb3f9556706d0012e8dd01c4bb680107
auroratide <tf.auroratide@gmail.com>
2020-01-19 15:10:07 -0600

    Better debugging

--- Assets/Debugging/Debugging.cs
+++ Assets/Debugging/Debugging.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,5 +7,14 @@ public class Debugging : MonoBehaviour
     public static void Log(object text)
     {
         GameObject.Find("DebugText").GetComponent<Text>().text = text.ToString();
+    }
+
+    public static void Use(Action action)
+    {
+        try {
+            action();
+        } catch(Exception e) {
+            GameObject.Find("DebugText").GetComponent<Text>().text = e.Message + "\n" + e.StackTrace;
+        }
     }
 }
