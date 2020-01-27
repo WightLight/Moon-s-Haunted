4e02763b473df9be31ab16c86eb7c71b42073e6f
Wisp <chromewisp@gmail.com>
2020-01-26 20:16:47 -0600

    Added mid-experimental Game Over pop-up (please solve prolly with Aspect Ratio Fitter component)

--- Assets/Zapper/Zapper.cs
+++ Assets/Zapper/Zapper.cs
@@ -8,22 +8,36 @@ public class Zapper : MonoBehaviour
     public AudioSource audio;
     public Animator animator;
 
+    public int heatLevel = 0;
+    public int maxHeat = 100;
+
     void Update()
     {
         if (IsFirstTouchDetected())
         {
-            audio.Play();
-            animator.Play("Shoot", 0, 0);
-            RaycastHit hit;
-            Ray ray = camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
-
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Reload"))
             {
-                Transform objectHit = hit.transform;
+                audio.Play();
+                animator.Play("Shoot", 0, 0);
 
-                if (objectHit.GetComponent<Ghost>() != null)
+                heatLevel += 10;
+                if (heatLevel >= maxHeat)
                 {
-                    objectHit.GetComponent<Ghost>().Zap();
+                    heatLevel = 0;
+                    animator.Play("Reload", 0, 0);
+                }
+
+                RaycastHit hit;
+                Ray ray = camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+                {
+                    Transform objectHit = hit.transform;
+
+                    if (objectHit.GetComponent<Ghost>() != null)
+                    {
+                        objectHit.GetComponent<Ghost>().Zap();
+                    }
                 }
             }
         }
